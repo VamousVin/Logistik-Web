636a3e447ad4b6f610eb6edc604a0b7037bcd2ae
VamousVin <gamersgg568@gmail.com>
2023-04-20 17:35:12 +0800

    memodifikasi hamburger menu

--- main.js
+++ main.js
@@ -5,14 +5,11 @@ const menu = document.querySelector("#menu");
 // memberikan perintah pada element body
 document.body.addEventListener("click", (e) => {
   if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
-    menu.classList.remove("block");
-    menu.classList.add("hidden");
+    menu.classList.remove("active");
   }
 });
 
 // menambahkan perintah pada element hamburger
-hamburger.addEventListener("click", () => {
-  // menambahkan fungsi toggle menu
-  menu.classList.toggle("hidden");
-  menu.classList.toggle("block");
+hamburger.addEventListener("click", function () {
+  menu.classList.toggle("active");
 });
